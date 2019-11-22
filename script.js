224ffe98fa66d29743cc26f4abb9f654739b127f
ivyrose <iliana.vargas@northwestern.edu>
2019-11-21 18:31:25 -0600

    adding html and css from gitlab

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * array of strings, "9AM etc."
+ * create a for loop, for each item in the array make a row in the HTML and append to the page
+ * get something to appear, add the times
+ * row with three columns
+ * 
+ *
+ * 
+ */
+
+var hours = ["9AM", "10 AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+var i;
+for (i = 0; i < hours.length; i++) {
+    console.log(hours[i])
+}
