55f43007c0b3bd59d8b8086b75e4f4bc2316b3ed
ivyrose <iliana.vargas@northwestern.edu>
2019-11-23 18:47:42 -0600

    small changes

--- script.js
+++ script.js
@@ -11,17 +11,38 @@
 $(document).ready(function() {
 
  var hours = ["9AM", "10 AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-
+ 
 var i;
 for (i = 0; i < hours.length; i++) {
-    var hoursRow = $("<row>");
+    //creating the row for the hours
+    var hoursRow = $("<div>");   
+    hoursRow.addClass("hours-row row hours-row-color");
+    hoursRow.attr("data-letter", hours[i]);
 
-        
-        hoursRow.addClass("hours-row row hours-row-color");
-        hoursRow.attr("data-letter", hours[i]);
-        hoursRow.text(hours[i]);
-        $("#row").append(hoursRow);
+    //column 1
+    var timeColumn = $("<div>");   
+    timeColumn.addClass("col-sm");
+    timeColumn.text(hours[i])
+    $(hoursRow).append(timeColumn);
+
+    //column 2
+    var textColumn = $("<div>");
+    textColumn.addClass("col-lg");
+    $(hoursRow).append(textColumn);
+
+    //column 3
+    var saveColumn = $("<div>");
+    saveColumn.addClass("col-sm");
+    $(hoursRow).append(saveColumn);
+
+
+    //last step
+    $(".container").append(hoursRow)
+    
 
 }
+
+
+
 });
 
