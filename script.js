904a82cffef41dbcec2a8730549cc8f7ff13b8d6
ivyrose <iliana.vargas@northwestern.edu>
2019-11-27 19:18:40 -0600

    added a text box

--- script.js
+++ script.js
@@ -17,36 +17,40 @@ for (i = 0; i < hours.length; i++) {
     //creating the row for the hours
     var hoursRow = $("<div>");   
     hoursRow.addClass("hours-row row hours-row-color");
-    hoursRow.attr("data-letter", hours[i]);
+    hoursRow.attr("data-index", hours[i]);
 
     //column 1
     var timeColumn = $("<div>");   
     timeColumn.addClass("col-sm");
-    timeColumn.text(hours[i])
+    timeColumn.text(hours[i]);
     $(hoursRow).append(timeColumn);
 
     //column 2
     var textColumn = $("<div>");
     textColumn.addClass("col-lg");
     $(hoursRow).append(textColumn);
+    
 
     //column 3
     var saveColumn = $("<div>");
     saveColumn.addClass("col-sm");
     $(hoursRow).append(saveColumn);
 
+    var textArea = $("<textarea></textarea>");
+    textArea.addClass("col-lg");
+    $(textColumn).append(textArea)
+
 
     //last step
     $(".container").append(hoursRow)
+
     
-
 }
-
 
 
 });
 
 //display date
-var today = moment().format("MMMM Do YYYY");
-$("#currentDay").text(today);
+var currentDate = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentDate);
 
